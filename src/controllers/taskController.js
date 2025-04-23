1ed98bab7bf0529b1c5c5e3d81c134957225d4c0
unknown <seemapal241@GMAIL.com>
2025-04-23 16:18:46 +0530

    work on update api with pagination and title and status

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,57 +1,103 @@
 
 import Task from '../models/taskModel.js';
-
+import {taskValidationSchema} from '../validations/taskValidation.js'
 export async function create(req, res) {
-    const taskData = req.body
-     console.log(taskData);
-    if (!taskData.title) {
-       return res.send({ message: "title is missing" })
-    } 
+    const { error } = taskValidationSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+    const taskData= req.body
     const taskPayload = new Task(taskData);
     await taskPayload.save();
-    return res.status(201).send({message:"create newtask successfully"});
+    return res.status(201).send({ message: "create newtask successfully" });
 
 
 }
 
-export async function getAssignedTask(req,res){
-    try{
-        const assignedTo_title=req.query.title || "";
-        const assignedTo= req.params.assignedTo 
-        if(!assignedTo){
-            return res.status(400).send({message: "AssignedTo task  is missing"})
-          }
-        let filterObject= {}
-        if(assignedTo_title){
-          filterObject.title= assignedTo_title
+export async function getAssignedTask(req, res) {
+    try {
+        const taskTitle = req.query.title || "";
+        const assignedTo = req.params.assignedTo
+        const statusTask = req.query.status
+        
+        
+        if (!assignedTo) {
+            return res.status(400).send({ message: "AssignedTo task  is missing" })
         }
+        let filterObject = {}
+        if (taskTitle) {
+            filterObject.title = taskTitle
+        }
+        if(assignedTo){
+            filterObject.assignedTo = assignedTo
+        }
+       
+        if (statusTask) {
+            filterObject.status = statusTask;
+        }
+
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 10;
         const skip = (page - 1) * limit;
-        const result = await Task.find({assignedTo}).skip(skip).limit(limit)
+        const result = await Task.find(filterObject).skip(skip).limit(limit)
         console.log(result);
 
-        if (result.length>0) {
+        if (result.length > 0) {
 
             res.status(200).send({
-              message: 'Task Data found',
-              data: result,
-              count: result.length,
-              totalItems:await Task.countDocuments(),
-              totalPages: Math.ceil(await Task.countDocuments() / limit),
-              currentPage: page
+                message: 'Task Data found',
+                data: result,
+                count: result.length,
+                totalItems: await Task.countDocuments(),
+                totalPages: Math.ceil(await Task.countDocuments() / limit),
+                currentPage: page
             })
         }
-            else{
-            res.status(200).send({message:"data found",data:result})
+        else {
+            res.status(200).send({ message: "data found", data: result })
         }
     }
-    catch(err){
-        res.status(500).send({message:"something went wrong",error:err.message})
+    catch (err) {
+        res.status(500).send({ message: "something went wrong", error: err.message })
     }
 
 }
 
 
+export async function deleteTask(req, res) {
 
-    
+    const taskId = req.params.id
+    if (!taskId) {
+        return res.status(400).send({ message: "TaskID is missing" })
+    }
+    const result = await Task.deleteOne({ _id: taskId })
+    console.log(result);
+
+    if (result.deletedCount == 0) {
+        res.status(500).send({ message: "Something went wrong unable to delete" })
+    } else {
+        res.status(200).send({ message: "task is  deleted sucessfully" })
+    }
+
+}
+
+export async function updateTask(req, res) {
+    const taskId = req.params.id
+    if (!taskId) {
+        return res.status(400).send({ message: "TaskID is missing" })
+    }
+    const { error } = taskValidationSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+    }
+    try {
+        const result = await Task.updateOne({ _id: taskId }, {$set: req.body})
+        console.log(result);
+        res.status(200).send({message: "Updated Sucessfuly"}, result)
+        
+    }
+    catch(err){
+        res.send(500).send({message:"Error", error: err.message})
+    }
+
+}
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import {create, getAssignedTask} from '../controllers/taskController.js';
+import {create, deleteTask, getAssignedTask, updateTask} from '../controllers/taskController.js';
 import authMiddleware from '../middlerware/authorization.js';
 
-router.post('/task', authMiddleware, create)
+router.post('/', authMiddleware, create)
 router.get('/:assignedTo', authMiddleware, getAssignedTask)
-
+router.delete('/:id', authMiddleware, deleteTask)
+router.put('/:id',authMiddleware,updateTask)
 export default router;
